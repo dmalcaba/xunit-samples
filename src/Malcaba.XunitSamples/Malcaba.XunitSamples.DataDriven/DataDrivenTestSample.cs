d53d224e222f62eb1fea811a32688a5d778d6dea
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-09-26 11:09:35 -0700

    Add data driven test sample using inline data

---
+++ src/Malcaba.XunitSamples/Malcaba.XunitSamples.DataDriven/DataDrivenTestSample.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Malcaba.XunitSamples.DataDriven
+{
+    public class DataDrivenTestSample
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        public void AddTest(int a, int b, int expected)
+        {
+            var calculator = new Calculator();
+            var actual = calculator.Add(a, b);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
