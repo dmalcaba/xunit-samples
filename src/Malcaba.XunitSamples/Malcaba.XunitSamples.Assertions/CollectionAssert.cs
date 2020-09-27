9858165f366ff89d246d77953564e351e6e657ec
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-09-26 17:22:51 -0700

    Add additional collection assert examples

--- src/Malcaba.XunitSamples/Malcaba.XunitSamples.Assertions/CollectionAssert.cs
+++ src/Malcaba.XunitSamples/Malcaba.XunitSamples.Assertions/CollectionAssert.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Malcaba.XunitSamples.Assertions
 {
     public class CollectionAssert
     {
+        private readonly ITestOutputHelper _output;
+
+        public CollectionAssert(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void GetBookTest1()
         {
@@ -23,6 +33,100 @@ namespace Malcaba.XunitSamples.Assertions
 
             Assert.Empty(result);
         }
+
+        [Fact]
+        public void GetBookTest3()
+        {
+            var repo = new BookRepository();
+            var result = repo.GetBookByAuthor("Stephen King");
+
+            Assert.NotEmpty(result);
+
+            _output.WriteLine("Test that the Titles are in the expected order.");
+            
+            Assert.Collection(result.Select(x => x.Title),
+                    item => Assert.Equal("Cujo", item),
+                    item => Assert.Equal("Mr. Mercedes", item)
+                );
+
+            // normally you would only have one assert per test 
+            // for demo purpose only
+            _output.WriteLine("Test that the Author name contains 'King'.");
+
+            Assert.All(result,
+                    item => item.Author.Contains("King")
+                );
+        }
+
+        [Fact]
+        public void GetBookTest4()
+        {
+            var repo = new BookRepository();
+
+            var result = repo.GetBookByAuthor("Stephen King");
+            var expectedBooks = repo.GetBookByAuthor("Stephen King");
+
+            // Although the results are actually the same
+            // It will not be equal using the default comparer
+
+            Assert.NotEqual(expectedBooks, result);
+
+            // See AssertWith extension method below;
+            result.AssertWith(expectedBooks, (e, a) =>
+            {
+                Assert.Equal(e.Id, a.Id);
+                Assert.Equal(e.Author, e.Author);
+                Assert.Equal(e.Title, e.Title);
+            });
+        }
+
+        [Fact]
+        public void CompareTwoCollections()
+        {
+            string[] collection1 = { "one", "two", "three" };
+            var collection2 = new List<string> { "one", "two", "three" };
+
+            Assert.Equal(collection1, collection2.ToArray());
+        }
+
+        [Fact]
+        public void CompareTwoCollections2()
+        {
+            var collection1 = new Dictionary<int, string>
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" }
+            };
+
+            var collection2 = new Dictionary<int, string>
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" }
+            };
+
+            Assert.Equal(collection1, collection2);
+
+            var collection3 = new Dictionary<int, string>
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 4, "four" }
+            };
+
+            Assert.NotEqual(collection1, collection3);
+
+        }
+
+    }
+
+    public static class TestExtensions
+    {
+        public static void AssertWith<TExpected, TActual>(this IEnumerable<TActual> actual, IEnumerable<TExpected> expected, Action<TExpected, TActual> inspector)
+        {
+            Assert.Collection(actual, expected.Select(e => (Action<TActual>)(a => inspector(e, a))).ToArray());
+        }
     }
 
     public class BookRepository
@@ -31,6 +135,7 @@ namespace Malcaba.XunitSamples.Assertions
         {
             return Books
                 .Where(x => x.Author == author)
+                .OrderBy(x => x.Title)
                 .ToList();
         }
 
