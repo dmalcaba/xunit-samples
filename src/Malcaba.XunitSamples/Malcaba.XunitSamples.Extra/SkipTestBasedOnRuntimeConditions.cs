8c9187c49924aba2dc74b94109d8aecbd11b9a9a
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-09-29 10:35:58 -0700

    Add sample skip test based on runtime conditions

---
+++ src/Malcaba.XunitSamples/Malcaba.XunitSamples.Extra/SkipTestBasedOnRuntimeConditions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Malcaba.XunitSamples.Extra
+{
+    /// <summary>
+    /// https://josephwoodward.co.uk/2019/01/skipping-xunit-tests-based-on-runtime-conditions
+    /// </summary>
+    public class SkipTestBasedOnRuntimeConditions
+    {
+        [IgnoreOnAppVeyorLinuxFact]
+        public void MyTest()
+        { 
+        }
+
+    }
+
+    /// <summary>
+    /// Extend the FactAttribute class and set the Skip Property if condition is met
+    /// </summary>
+    public sealed class IgnoreOnAppVeyorLinuxFact : FactAttribute
+    {
+        public IgnoreOnAppVeyorLinuxFact()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && IsAppVeyor())
+            {
+                Skip = "Ignore on Linux when run via AppVeyor";
+            }
+        }
+
+        private static bool IsAppVeyor()
+            => Environment.GetEnvironmentVariable("APPVEYOR") != null;
+    }
+}
