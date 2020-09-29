b41b3b9e3128beb869775fa54639728cd7f647ab
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-09-29 15:52:13 -0700

    Add more data driven test samples

--- src/Malcaba.XunitSamples/Malcaba.XunitSamples.DataDriven/DataDrivenTestSample.cs
+++ src/Malcaba.XunitSamples/Malcaba.XunitSamples.DataDriven/DataDrivenTestSample.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Malcaba.XunitSamples.DataDriven
@@ -16,6 +16,53 @@ namespace Malcaba.XunitSamples.DataDriven
 
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void CanAddTheoryMemberDataProperty(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// https://andrewlock.net/creating-parameterised-tests-in-xunit-with-inlinedata-classdata-and-memberdata/
+        /// </summary>
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { 1, 2, 3 },
+                new object[] { -4, -6, -10 },
+                new object[] { -2, 2, 0 },
+                new object[] { int.MinValue, -1, int.MaxValue },
+            };
+
+        [Theory]
+        [MemberData(nameof(StronglyTypedData))]
+        public void CanAddTheoryDataProperty(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Strongly-typed version
+        /// https://andrewlock.net/creating-strongly-typed-xunit-theory-test-data-with-theorydata/
+        /// </summary>
+        public static TheoryData<int, int, int> StronglyTypedData =>
+            new TheoryData<int, int, int>
+                {
+                    { 1, 2, 3 },
+                    { -4, -6, -10 },
+                    { -2, 2, 0 },
+                    { int.MinValue, -1, int.MaxValue }
+                };
     }
 
     public class Calculator
