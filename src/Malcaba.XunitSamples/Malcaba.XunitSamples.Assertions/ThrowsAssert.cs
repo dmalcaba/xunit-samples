c26d1fb91f3ce67a22914ac1a8d62ed89194ed95
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2021-07-06 09:34:46 -0700

    Add throws assert examples

---
+++ src/Malcaba.XunitSamples/Malcaba.XunitSamples.Assertions/ThrowsAssert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace Malcaba.XunitSamples.Assertions
+{
+    public class ThrowsAssert
+    {
+        [Fact]
+        public void AssertThrowsExample()
+        {
+            var dividend = 4;
+            var divisor = 0;
+
+            // you may capture the exception like this while asserting
+            // assert expects exact match and not derived
+            var ex = Assert.Throws<DivideByZeroException>(() => dividend / divisor);
+
+            // so you can also assert message returned
+            Assert.Equal("Attempted to divide by zero.", ex.Message);
+        }
+
+        [Fact]
+        public void AssertThrowsAnyExample()
+        {
+            var dividend = 4;
+            var divisor = 0;
+
+            // if you want to just assert derived exceptions
+            Assert.ThrowsAny<Exception>(() => dividend / divisor);
+        }
+
+    }
+
+    
+}
